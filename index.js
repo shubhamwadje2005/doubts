8c7404ebf63271872d948faa2507fc2aff31ed63
SHUBHAM WADJE <shubhamwadje2005@gmail.com>
2025-03-20 16:39:18 +0530

    initial commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ require("dotenv").config()
 const path = require("path")
 const app = express()
 
+app.use(express.static("dist"))
 app.use(express.json())
 app.use(cors({ origin: true, credentials: true }))
 app.use(cookieParser())
